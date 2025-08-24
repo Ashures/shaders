1243f0ba0616ab5d8d480ae85e93327c8f1e51c7
Ashures <ashuresyt@gmail.com>
2025-08-24 22:05:35 +1200

    feat: Initial commit

---
+++ _Scenes/CameraShader3D/camera_shader_3d.gd
@@ -0,0 +1,20 @@
+class_name CameraShader3D extends Camera3D
+
+@onready
+var screen_mesh: MeshInstance3D = $SpatialMesh3D
+
+@export var shader: Shader
+
+@export_color_no_alpha var colors: Array[Color]
+
+func _ready() -> void:
+	var new_mesh: QuadMesh = QuadMesh.new()
+	new_mesh.size = Vector2(2.0, 2.0)
+	new_mesh.flip_faces = true
+	
+	var new_material: ShaderMaterial = ShaderMaterial.new()
+	new_material.shader = shader
+	new_material.set_shader_parameter("light_colors", colors)
+	
+	screen_mesh.mesh = new_mesh
+	screen_mesh.material_override = new_material
