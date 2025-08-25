8b80eea405748a4f61a4fccd87e5d7baf9b55208
Ashures <ashuresyt@gmail.com>
2025-08-25 19:45:40 +1200

    feat: Improved docs in basics.gdshaderinc and added lerp helper function

--- _Scenes/CameraShader3D/camera_shader_3d.gd
+++ _Scenes/CameraShader3D/camera_shader_3d.gd
@@ -5,7 +5,7 @@ var screen_mesh: MeshInstance3D = $SpatialMesh3D
 
 @export var shader: Shader
 
-@export_color_no_alpha var colors: Array[Color]
+@export_color_no_alpha var background_color: Color
 
 func _ready() -> void:
 	var new_mesh: QuadMesh = QuadMesh.new()
@@ -14,7 +14,7 @@ func _ready() -> void:
 	
 	var new_material: ShaderMaterial = ShaderMaterial.new()
 	new_material.shader = shader
-	new_material.set_shader_parameter("light_colors", colors)
+	new_material.set_shader_parameter("background_color", background_color)
 	
 	screen_mesh.mesh = new_mesh
 	screen_mesh.material_override = new_material
