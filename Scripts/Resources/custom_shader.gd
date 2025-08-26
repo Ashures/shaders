baa5160c043f0a5387d2477683f148ac81f01eae
Ashures <ashuresyt@gmail.com>
2025-08-26 23:48:58 +1200

    feat: Converted shader into multi-pass shaders

---
+++ Scripts/Resources/custom_shader.gd
@@ -0,0 +1,5 @@
+class_name CustomShader extends Resource
+
+@export var shader_name: String
+@export var shader: Shader
+@export var settings: Dictionary
--- _Scenes/CameraShader3D/camera_shader_3d.gd
+++ _Scenes/CameraShader3D/camera_shader_3d.gd
@@ -1,20 +1,25 @@
 class_name CameraShader3D extends Camera3D
 
-@onready
-var screen_mesh: MeshInstance3D = $SpatialMesh3D
-
-@export var shader: Shader
-
-@export_color_no_alpha var background_color: Color
+@onready var shader_parent: Node = $Shaders
+@export var shaders: Array[CustomShader]
 
 func _ready() -> void:
-	var new_mesh: QuadMesh = QuadMesh.new()
-	new_mesh.size = Vector2(2.0, 2.0)
-	new_mesh.flip_faces = true
-	
-	var new_material: ShaderMaterial = ShaderMaterial.new()
-	new_material.shader = shader
-	new_material.set_shader_parameter("background_color", background_color)
-	
-	screen_mesh.mesh = new_mesh
-	screen_mesh.material_override = new_material
+	for i in range(len(shaders)):
+		var new_mesh: QuadMesh = QuadMesh.new()
+		new_mesh.size = Vector2(2.0, 2.0)
+		new_mesh.flip_faces = true
+		
+		var new_material: ShaderMaterial = ShaderMaterial.new()
+		new_material.shader = shaders[i].shader
+		new_material.render_priority = i
+		
+		for key in shaders[i].settings.keys():
+			new_material.set_shader_parameter(key, shaders[i].settings[key])
+		
+		var new_mesh_instance: MeshInstance3D = MeshInstance3D.new()
+		shader_parent.add_child(new_mesh_instance)
+		new_mesh_instance.extra_cull_margin = 1.0
+		new_mesh_instance.mesh = new_mesh
+		new_mesh_instance.material_override = new_material
+		new_mesh_instance.name = shaders[i].shader_name
+		
